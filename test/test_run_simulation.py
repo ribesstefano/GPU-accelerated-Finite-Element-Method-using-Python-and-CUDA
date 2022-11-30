c5272382d00ea30018cb8c5805ed756d2fe28121
Stefano Ribes <stefanor@ribes.got.gaisler.com>
2022-11-30 18:26:15 +0100

    Written CPU profiling for sparse solvers

--- test/test_run_simulation.py
+++ test/test_run_simulation.py
@@ -2,10 +2,11 @@ from BasicFEM import *
 
 import cProfile
 import pstats
-import time
 import numpy as np
 import scipy.sparse
 import scipy.sparse.linalg
+import time
+import timeit
 
 def test_run_simulation():
     # Define grid and cells
@@ -83,7 +84,7 @@ def test_run_simulation():
 
     A = scipy.sparse.csr_matrix(K[free_dofs, :][:, free_dofs], dtype=np.float32, copy=True)
     Kb = scipy.sparse.csr_matrix(-K[free_dofs, :][:, prescribed_dofs], dtype=np.float32, copy=True)
-    b = Kb @ f[prescribed_dofs]
+    b = Kb @ a[prescribed_dofs]
     a[free_dofs] = scipy.sparse.linalg.spsolve(A, b)
     # a[free_dofs] = scipy.sparse.linalg.spsolve(K[free_dofs, :][:, free_dofs], -K[free_dofs, :][:, prescribed_dofs] @ a[prescribed_dofs])
 
@@ -93,7 +94,7 @@ def test_run_simulation():
     stats = pstats.Stats(profiler).sort_stats('cumtime')
     # Print the 10 (inclusive) most expensive functions
     stats.print_stats(100)
-    stats.dump_stats('../program.prof')
+    stats.dump_stats('program.prof')
 
 
 def test_run_simulation_plate_with_hole():
@@ -143,5 +144,82 @@ def test_run_simulation_plate_with_hole():
     prescribed_dofs = np.concatenate((bottom_dofs[:, 1], left_dofs[:,0], top_dofs[:,1]))
     free_dofs = np.setdiff1d(range(dh.ndofs_total(grid)), prescribed_dofs)
 
-    a[free_dofs] = scipy.sparse.linalg.spsolve(K[free_dofs, :][:, free_dofs], -K[free_dofs, :][:, prescribed_dofs] @ f[prescribed_dofs])
+    a[free_dofs] = scipy.sparse.linalg.spsolve(K[free_dofs, :][:, free_dofs], -K[free_dofs, :][:, prescribed_dofs] @ a[prescribed_dofs])
 
+
+def profile_solvers():
+    dh = DofHandler(2)
+    E = 200e3
+    nu = 0.3
+    material = Elasticity(E, nu)
+    thickness = 1.0
+    weak_form = MomentumBalance(material, thickness)
+    element = CST()
+
+    def k_assembly(lcar=0.1):
+        grid = generate_grid(lcar)
+        I, J = dh.sparsity_pattern(grid)
+        K = scipy.sparse.csr_matrix((np.zeros(len(I)), (I, J)))
+        f = np.zeros(len(I))
+
+        ndofs_cell = dh.ndofs_per_cell(grid)
+        ke = np.zeros((ndofs_cell, ndofs_cell))
+        re = np.zeros(ndofs_cell)
+        dofs = np.empty(ndofs_cell, np.int32)
+        xe = np.zeros((grid.nnodes_per_cell(), 2))
+        a = np.zeros(dh.ndofs_total(grid))
+
+        bottom_dofs = dh.getdofs(grid.nodesets['bottom'])
+        left_dofs = dh.getdofs(grid.nodesets['left'])
+        top_dofs = dh.getdofs(grid.nodesets['top'])
+
+        a[top_dofs[:,1]] = -0.1
+
+        for cellid in range(len(grid.cells)):
+            grid.getcoordinates(xe, cellid)
+            dh.celldofs(dofs, grid, cellid)
+            ke.fill(0.0)
+            re.fill(0.0)
+            ue = a[dofs] # not relevant as input for linear elasticity
+            weak_form.element_routine(ke, re, element, xe, ue)
+            # probably there is a better way for this
+            for (i, dof_i) in enumerate(dofs):
+                for (j, dof_j) in enumerate(dofs):
+                    K[dof_i, dof_j] += ke[i,j]
+                f[dof_i] += re[i]
+
+        prescribed_dofs = (bottom_dofs[:, 1], left_dofs[:,0], top_dofs[:,1])
+        prescribed_dofs = np.concatenate(prescribed_dofs)
+        free_dofs = np.setdiff1d(range(dh.ndofs_total(grid)), prescribed_dofs)
+        return a, K, f, free_dofs, prescribed_dofs, grid
+
+    n_runs = 20
+    lcars = [0.5, 0.1, 0.05, 0.01]
+    solvers = [
+            ('spsolve', scipy.sparse.linalg.spsolve),
+            ('bicg', scipy.sparse.linalg.bicg),
+            ('bicgstab', scipy.sparse.linalg.bicgstab),
+            ('cg', scipy.sparse.linalg.cg),
+            ('cgs', scipy.sparse.linalg.cgs),
+            ('gmres', scipy.sparse.linalg.gmres),
+            ('lgmres', scipy.sparse.linalg.lgmres),
+            ('minres', scipy.sparse.linalg.minres),
+            ('qmr', scipy.sparse.linalg.qmr),
+            ('gcrotmk', scipy.sparse.linalg.gcrotmk),
+            ('tfqmr', scipy.sparse.linalg.tfqmr),
+        ]
+    with open('solvers_profiling.csv', 'w') as fp:
+        fp.write(f'solver_name,lcar,n_nodes,n_cells,n_runs,cpu/gpu,t[s]\n')
+        for lcar in lcars:
+            for solver_name, solver in solvers:
+                a, K, f, free_dofs, prescribed_dofs, grid = k_assembly(lcar)
+                A = K[free_dofs, :][:, free_dofs]
+                b = -K[free_dofs, :][:, prescribed_dofs] @ a[prescribed_dofs]
+                t = timeit.timeit(stmt=lambda: solver(A, b), number=n_runs)
+                n_nodes = len(grid.nodes)
+                n_cells = len(grid.cells)
+                print(f'[Mesh size: {lcar}] {solver_name}: {t / n_runs:.4f} s')
+                fp.write(f'{solver_name},{lcar},{n_nodes},{n_cells},{n_runs},cpu,{t / n_runs:.4f}\n')
+
+if __name__ == '__main__':
+    profile_solvers()
